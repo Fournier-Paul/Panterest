a5bc9c67d1f1edf2280580a93355fb5d6b83a74e
Paul1465 <paul.fournier.3314@gmail.com>
2020-11-28 18:01:59 +0100

    Add bootstrap & use form themes & fix little bug

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -58,7 +58,7 @@ class Pin
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    public function setTitle(?string $title): self
     {
         $this->title = $title;
 
@@ -70,7 +70,7 @@ class Pin
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    public function setDescription(?string $description): self
     {
         $this->description = $description;
 
