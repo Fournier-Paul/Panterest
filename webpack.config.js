9787c4887cb877c95f70272133788de9d7e6f6fc
Paul1465 <paul.fournier.3314@gmail.com>
2020-12-03 12:54:59 +0100

    Cpy files rom assets/images to public/build via Encore and style homepage

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,11 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/admin.js')
+    .copyFiles({
+        from: './assets/images',
+        to: '[path][name].[hash:8].[ext]',
+            context: './assets'
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
