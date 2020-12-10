89578484318a4884fc6886798d15f514745d0fdf
Paul1465 <paul.fournier.3314@gmail.com>
2020-12-10 13:30:19 +0100

    Use POST method for logout and add CSRF protection

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,14 +8,14 @@ use App\Entity\Traits\Timestampable;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Validator\Constraint as Assert;
 
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\HasLifecycleCallbacks()
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields="email", message="There is already an account with this email")
  */
 class User implements UserInterface
 {
