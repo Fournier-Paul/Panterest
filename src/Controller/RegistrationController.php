b2a9bebaeaa81cdb3e5bbdf57bb54da91675ecb1
Paul1465 <paul.fournier.3314@gmail.com>
2020-12-10 20:00:01 +0100

    Redirect logged in users to home if trying to access register page and also redirect to home if verification link is invalid

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -90,9 +90,9 @@ class RegistrationController extends AbstractController
         try {
             $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
         } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $exception->getReason());
+            $this->addFlash('error', $exception->getReason());
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_home');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
