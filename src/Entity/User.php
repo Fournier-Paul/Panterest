936a0eae240ccd6d06c0fd2f6f5d31ecfe16399c
Paul1465 <paul.fournier.3314@gmail.com>
2020-12-11 15:37:09 +0100

    Add resetPassword feature

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,6 +14,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="users")
  * @ORM\HasLifecycleCallbacks()
  * @UniqueEntity(fields="email", message="There is already an account with this email")
  */
