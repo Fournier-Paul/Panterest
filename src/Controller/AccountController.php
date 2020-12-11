57a8504746b983b3f5af2f1e0dd6d9a7fde0ccf3
Paul1465 <paul.fournier.3314@gmail.com>
2020-12-11 20:07:40 +0100

    fix bugs and change password form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,26 +2,30 @@
 
 namespace App\Controller;
 
+use App\Form\UserFormType;
 use App\Form\ChangePasswordFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+/**
+ * @Route("/account")
+ */
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="app_account", methods="GET")
+     * @Route("", name="app_account", methods="GET")
      */
     public function show(): Response
     {
         return $this->render('account/show.html.twig');
     }
     /**
-     * @Route("/account/edit", name="app_account_edit", methods={"GET","POST"})
+     * @Route("/edit", name="app_account_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, EntityManagerInterface $em): Response
     {
@@ -45,27 +49,31 @@ class AccountController extends AbstractController
     }
 
     /**
-     * @Route("/account/change-password", name="app_account_change_password", methods="GET|POST")
+     * @Route("/change-password", name="app_account_change_password", methods="GET|POST")
      */
     public function changePassword(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = $this->getUser();
 
-        $form = $this->createForm(ChangePasswordFormType::class);
+        $form = $this->createForm(ChangePasswordFormType::class, null, [
+            'current_password_is_required' => true
+        ]);
 
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid())
         {
             $user->setPassword(
-                $passwordEncoder->encodePassword($user, $form['newPassword']->getData())
+                $passwordEncoder->encodePassword($user, $form['plainPassword']->getData())
             );
+
             $em->flush();
-            $this->addFlash('success', 'PAssword updated successfully');
+
+            $this->addFlash('success', 'Password updated successfully');
+
             return $this->redirectToRoute('app_account');
+
         }
-
-
         return $this->render('account/change_password.html.twig', [
             'form' => $form->createView()
         ]);
--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -3,10 +3,11 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class UserFormType extends AbstractType
 {
